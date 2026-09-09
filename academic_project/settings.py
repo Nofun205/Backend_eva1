bb05e031f6a2326498eb2b031dab1b017ab3d45b
Nofun205 <diegoyevenes5@gmail.com>
2026-09-09 09:49:19 -0300

    Docs: Comentar las dependencias en settings.py

--- academic_project/settings.py
+++ academic_project/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'academic',
+    
+    # --- Agregados para el Proyecto ---
+    'rest_framework',  # Dependencia solicitada (Django REST Framework)
+    'academic',        # Nuestra aplicación principal
 ]
 
 MIDDLEWARE = [
